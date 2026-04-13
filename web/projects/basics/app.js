f0859013218058e26e0c93c00ff279c778ad1085
Barbi Radi <barbi.radi.05@gmail.com>
2026-04-14 01:13:12 +0200

    practising javascript

--- web/projects/basics/app.js
+++ web/projects/basics/app.js
@@ -1,1 +1,16 @@
-console.log('Hello Word');
+//Define a variable to hold our experiment name
+let experiment = 'Stroop';
+let trialCountMax = 20;
+
+//TODO: Randomize colors
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+//At the halpway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+console.log(halfWayCount); //Expected: 20
+
---
+++ web/projects/basics/app2.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+alert('Hello again World!');
+prompt()
+
+function fahrenheitToCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9;
+    return celsius;
+}
+
+if (temp > 90) {
+    console.log('It is hot out!')
+}
