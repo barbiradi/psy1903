3b81db91f0ef21e4d4985b6fb873228a0a8ef758
Barbi Radi <barbi.radi.05@gmail.com>
2026-04-14 01:28:49 +0200

    practise javascript (data types)

--- web/projects/basics/app.js
+++ web/projects/basics/app.js
@@ -1,7 +1,13 @@
+let welcomeMessage = `Welcome to our experiment. 
+Please read the instructions carefully`;
+
 //Define a variable to hold our experiment name
 let experiment = 'Stroop';
-let trialCountMax = 20;
+console.log(typeof experiment); //string
+console.log(experiment.charAt(0)); //'S'
 
+let trialCountMax = 20;
+console.log(typeof trialCountMax); //number
 //TODO: Randomize colors
 let colors = ['red', 'green', 'blue'];
 
@@ -14,3 +20,9 @@ let halfWayCount = trialCountMax / 2;
 
 console.log(halfWayCount); //Expected: 20
 
+//boolean values
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 > 15); //false
+console.log(10 < 15); //true
